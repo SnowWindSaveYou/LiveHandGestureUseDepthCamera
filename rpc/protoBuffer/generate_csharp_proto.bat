b94ff6f2961dc6dfc4d736bb7bbdb8d1231734fa
SnowWind <846590399@qq.com>
2022-01-14 17:33:20 +0800

    proto

--- rpc/protoBuffer/generate_csharp_proto.bat
+++ rpc/protoBuffer/generate_csharp_proto.bat
@@ -1,9 +1,9 @@
 @echo off
 
-set protos=*.proto
+set protos=./proto/*.proto
 
 ECHO generating c# files from proto
-for %%i in (%protos%) do (.\protoc_3.11.2.exe %%i --csharp_out=.\ --grpc_out=.\ --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe)
+for %%i in (./proto/%protos%) do (.\protoc_3.11.2.exe %%i --csharp_out=.\generated\ --grpc_out=.\generated\ --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe)
 
 ::translate line end to windows style
 @REM for %%i in (./*) do (.\unix2dos.exe ./%%i)
